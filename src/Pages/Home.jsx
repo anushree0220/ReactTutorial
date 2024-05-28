bf7e72aadcedf8a84404b705697c5ecee46528fd
Anushree Tiwari <tiwarianushree21@gmail.com>
2024-05-28 17:20:25 +0530

    hero section improvment

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,15 @@ import Card from '../Components/Card'
 function Home() {
   return (
     <div>
-        Home
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">Hello there</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
